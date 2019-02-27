55ce664e5c4af3d1866636522d6a81fab162556d
Ricardo Jorge Pais <31729218+rjpais@users.noreply.github.com>
2019-02-27 10:11:24 +0000

    Boxplot using ggplot2
    The script makes a simple boxplot of 2 input variables. In this script variable 1 was mapped as a function of variable 2 using the R function factor() inside the arguments of ggplot().  I also included the option of showing the data as dots on top of each barplot.
    This adds a small amount of random variation to the location of each point, useful for handling overplotting.

--- BoxPlotggplot2.R
+++ BoxPlotggplot2.R
@@ -4,10 +4,11 @@ library(ggplot2)
 #import data (data frame) from a csv file 
 data <-  read.csv (file.choose())
 
-# Generate a simple boxplot of variable2  mapped with variable 2 using ggplot2.
-BP <- ggplot(data, aes(x=as.factor(variableName1), y=variableName2)) + 
+# Generate a simple boxplot of variable 2 (y)  mapped with variable 1 (x) using ggplot2.
+BP <- ggplot(data, aes(x=as.factor(variableName1), y=variableName2)) +    
   geom_boxplot(fill="yellow", alpha=0.2) + 
   xlab("name of variable 1")
+
 # Showing the Box plot with the data overlaping
 BP + geom_jitter(shape=16, position=position_jitter(0.2), color = "darkblue")
 
